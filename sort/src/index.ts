85401dff80a0bcfe2feca4e97c5e694e18bbfa59
Uladzimir Markau <kitanokiller@gmail.com>
2020-11-13 14:55:15 +0300

    Add the CharactersCollection class.

--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from "./Sorter";
-import { NumbersCollection } from "./NumbersCollection";
+//import { NumbersCollection } from "./NumbersCollection";
+import { CharactersCollection } from "./CharactersCollection";
 
-const numbersCollection = new NumbersCollection([3, 10, -5, 0]);
+/* const numbersCollection = new NumbersCollection([3, 10, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
-console.log(numbersCollection.data);
+console.log(numbersCollection.data); */
+
+const charactersCollection = new CharactersCollection("Xaacbedx");
+const sorter = new Sorter(charactersCollection);
+sorter.sort();
+console.log(charactersCollection.data);
