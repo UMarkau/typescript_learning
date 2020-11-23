a5e1fddf7912045e701f388435a583a20f51b1fb
Uladzimir Markau <kitanokiller@gmail.com>
2020-11-23 15:55:58 +0300

    Add more examples of composition-based approach. Add a static method to the Summary class.

---
+++ stats/src/MatchData.ts
@@ -0,0 +1,12 @@
+import { MatchResult } from "./MatchResult";
+
+// A tupple
+export type MatchData = [
+  Date,
+  string,
+  string,
+  number,
+  number,
+  MatchResult,
+  string
+];
--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,5 +1,8 @@
 import { MatchReader } from "./MatchReader";
 import { CsvFileReader } from "./CsvFileReader";
+import { WinsAnalysis } from "./analyzers/WinsAnaliysis";
+import { Summary } from "./Summary";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 // 1) Create an object that satisfies the "DataReader" interface;
 const csvFileReader = new CsvFileReader("football.csv");
@@ -7,4 +10,11 @@ const csvFileReader = new CsvFileReader("football.csv");
 // 2) Create an instance of MatchReader and pass in something satisfying the "DataReader" interface.
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-console.log(matchReader.matches);
+
+const summary = new Summary(new WinsAnalysis("Man United"), new HtmlReport());
+summary.buildAndPrintReport(matchReader.matches);
+
+// OR
+
+const anotherSummary = Summary.winsAnalysisWithHtmlReport("Man United");
+// anotherSummary.buildAndPrintReport(matchReader.matches)
